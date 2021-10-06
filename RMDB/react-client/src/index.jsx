3a887e9fda2156254a86f07eda07d1bd51f65210
heniLtaief <heniltaief@gmail.com>
2021-10-06 11:27:14 +0100

    firstc

--- RMDB/react-client/src/index.jsx
+++ RMDB/react-client/src/index.jsx
@@ -1,15 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
-
+import axios from 'axios'
+import HomePage from './components/HomePage.jsx';
+import samplePosts from "./data/data"
 class App extends React.Component {
   constructor() {
     super();
     this.state = {
-      view: 'feed'
+      view: 'login',
+      // trailers:[],
+      currentTrailer: null,
+      samplePosts:samplePosts
     }
 
     this.changeView = this.changeView.bind(this);
+    this.handleTrailerItems= this.handleTrailerItems.bind(this)
   }
 
   changeView(option) {
@@ -17,6 +22,34 @@ class App extends React.Component {
       view: option
     });
   }
+
+getData(){
+  this.setState({
+    samplePosts: this.state.samplePosts
+  })
+}  
+
+// updateData(trailerId){
+//   axios.put(`/api/trailers/:trailerId/${trailerId}`,{})
+//   .then((response)=>{
+//     console.log(response)
+//   })
+// }
+
+ // updateData(blogId,newTitle){
+  //   var newTitle= prompt()
+  //   axios.put(`http://localhost:3000/api/:trailerId/${trailerId}`,{title: newTitle})
+  //   .then((response)=>{
+  //     console.log(response)
+  //   })
+  // }
+
+// deleteData(trailerId){
+//   axios.delete(`/api/trailers/:trailerId${trailerId}`)
+//   .then((response)=>{
+//     console.log(response)
+//   })
+// }
 
   renderView() {
     const view = this.state.view;
@@ -27,24 +60,33 @@ class App extends React.Component {
     else if(view==='signup') {
       return <SignUp />
     }
+    else if(view==='homepage') {
+      return <HomePage />
+    }
     else if(view==='watchList') {
       return <WatchList />
     }
   }
+  
 
+handleTrailerItems(trailer){
+this.setState({
+  currentTrailer: trailer,
+})
+}
 
   render() {
     return (
       <div>
         <div className="nav">
           <span className="logo"
-            onClick={() => this.changeView('feed')}>
-            RMDB
+            onClick={() => this.changeView('rmdb')}>
+              ЯMDb
           </span>
-          <span className={this.state.view === 'feed'
+          <span className={this.state.view === 'rmdb'
             ? 'nav-selected'
             : 'nav-unselected'}
-            onClick={() => this.changeView('kjhg')}>
+            onClick={() => this.changeView('towatch')}>
             WatchList
           </span>
           <span className="nav-unselected"  onClick={() => this.changeView('signin')}>
@@ -54,7 +96,8 @@ class App extends React.Component {
             Sign Up
           </span>
         </div>
-
+       
+       
         <div className="main">
           {this.renderView()}
         </div>
@@ -63,4 +106,4 @@ class App extends React.Component {
   }
 }
 
-ReactDOM.render(<App />, document.getElementById('blogmodo'));
+ReactDOM.render(<App />, document.getElementById('RMDb'));
